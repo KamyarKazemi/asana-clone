ca2db3665d932cabd8de404f442086bfa87b6f83
KamyarKazemi <151082088+KamyarKazemi@users.noreply.github.com>
2025-08-26 19:00:56 +0330

    1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,17 +16,17 @@ function Header() {
         className={`p-2 fixed left-5 bottom-110 rounded-2xl text-2xl  ${styles.header}`}
       >
         <div className="grid grid-cols-2 z-2000 items-center">
-          <div className="flex flex-col items-center justify-center gap-2">
+          <div className="flex flex-col items-center justify-center">
             <div className="flex mb-5">
               <FaBox />
             </div>
-            <div className="flex flex-col gap-3 mb-5">
+            <div className="flex flex-col gap-3">
               <FaAddressBook />
               <FaCodeMerge />
               <FaFileLines />
               <FaWandMagicSparkles />
             </div>
-            <div className="flex flex-col gap-3 mb-5">
+            <div className="flex flex-col gap-3">
               <FaBorderNone />
               <FaGear />
             </div>
