e6baf97a6aab3dc7f924e90633faed96ff5795f0
KamyarKazemi <151082088+KamyarKazemi@users.noreply.github.com>
2025-08-26 19:49:38 +0330

    1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,7 +25,24 @@ function Sidebar() {
   return (
     <>
       <aside className="fixed left-0 bottom-0 h-screen w-20 bg-gray-900 text-white flex flex-col items-center py-6 shadow-2xl rounded-r-2xl">
-        <div>gg</div>
+        <div className="flex flex-col gap-8 mt-6  w-full items-center">
+          {navItems.map((item) => (
+            <button
+              key={item.id}
+              onClick={() => setActive(item.id)}
+              className={`relative group p-3 rounded-xl transition-all duration-300 ${
+                active === item.id
+                  ? "bg-gray-700 text-cyan-700"
+                  : "hover:bg-gray-800"
+              }`}
+            >
+              {item.icon}
+              <span className="absolute left-16 top-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 pointer-events-none transition bg-gray-800 text-white text-sm px-2 py-1 rounded-md whitespace-nowrap shadow-lg">
+                {item.label}
+              </span>
+            </button>
+          ))}
+        </div>
       </aside>
     </>
   );
