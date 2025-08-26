5fd52e347f4ce7adc8587b42d17093fd857fd25d
KamyarKazemi <151082088+KamyarKazemi@users.noreply.github.com>
2025-08-26 18:26:26 +0330

    1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,35 @@
-import styles from "../css/HeaderStyles.modile.css";
+import styles from "../css/HeaderStyles.module.css";
+import {
+  FaBox,
+  FaAddressBook,
+  FaCodeMerge,
+  FaFileLines,
+  FaWandMagicSparkles,
+  FaBorderNone,
+  FaGear,
+} from "react-icons/fa6";
 
 function Header() {
   return (
     <>
-      <div className={` ${styles.header}`}>
-        <div>hhh</div>
+      <div className={`p-2 ${styles.header}`}>
+        <div className="fixed left-5 bottom-110 grid grid-cols-2 z-2000">
+          <div className="flex flex-col">
+            <div className="flex">
+              <FaBox />
+            </div>
+            <div className="flex flex-col">
+              <FaAddressBook />
+              <FaCodeMerge />
+              <FaFileLines />
+              <FaWandMagicSparkles />
+            </div>
+            <div className="flex">
+              <FaBorderNone />
+              <FaGear />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
